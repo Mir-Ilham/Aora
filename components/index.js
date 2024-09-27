77dd2bb3f8331eba846405a655acf831962b5a4a
Mir Ilham <mhmilham3@gmail.com>
2024-09-27 22:59:39 +0600

    Add info box component

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 import VideoCard from "./VideoCard";
 import FormField from "./FormField";
 import CustomButton from "./CustomButton";
-// import InfoBox from "./InfoBox";
+import InfoBox from "./InfoBox";
 import Loader from "./Loader";
 import Trending from "./Trending";
 import SearchInput from "./SearchInput";
@@ -11,7 +11,7 @@ export {
   VideoCard,
   FormField,
   CustomButton,
-  // InfoBox,
+  InfoBox,
   Loader,
   Trending,
   SearchInput,
