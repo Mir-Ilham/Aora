93da56652a7a4a3e4f697aae75087fdcf45eb0f4
Mir Ilham <mhmilham3@gmail.com>
2024-09-28 14:56:56 +0600

    Add update and delete icons

--- constants/icons.js
+++ constants/icons.js
@@ -11,6 +11,8 @@ import logout from "../assets/icons/logout.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
 import play from "../assets/icons/play.png";
+import del from "../assets/icons/delete.png";
+import update from "../assets/icons/update.png";
 
 export default {
   play,
@@ -26,4 +28,6 @@ export default {
   logout,
   eyeHide,
   eye,
+  del,
+  update,
 };
